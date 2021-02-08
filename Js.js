e56307454e7b978a4c263808f6d504beaa663bea
Harman <herman.nothero@gmail.com>
2021-02-08 22:06:39 +0300

    End version

---
+++ Js.js
@@ -0,0 +1,46 @@
+let dollor = 74;
+let RubleUser = prompt ("Число ваших рублей?");
+let BarterDollor = prompt ("Сколько долларов обменять на рубли?");
+let barter = (BarterDollor * 74 - RubleUser);
+let barterD = (BarterDollor * 34);
+let barterC = (barterD + RubleUser);
+console.log(barter);
+console.log(barterD);
+console.log(barterC);
+let itog = `Василий иванович имел у себя ${RubleUser}р и с этими ${RubleUser}р он пошел в обменный центр. На тот момент доллар стоил ${dollor}р к одному. И обменял Василий свои рубли на доллары обменяв ${RubleUser}р на ${BarterDollor}$ и остаток у него составлял ${barter}р но на следующий день он вернулся и обменял свои доллары назад на рубли получив назад ${barterC}р`;
+
+alert(itog);
+/* Для продолжения скрипта, нужно приписать "g" к "ito" строкой ниже*/
+console.log(ito);
+
+let a = 1956;
+
+let b = "Я-я";
+
+alert(b);
+
+console.log(b);
+
+let data = "28.02.1999";
+
+alert (data);
+
+console.log(data);
+
+const Name = "Курица";
+
+alert (Name);
+
+console.log(Name);
+
+let NameUser = prompt ("Ваше имя?");
+let PhoneUser = prompt ("Ваш номер телефона?");
+let AgeUser = prompt ("Ваш возраст?");
+let CityUser = prompt ("Ваш город?");
+let AdresUser = prompt ("Ваш адрес?");
+
+let present = `Здраствуйте ${NameUser}. Вам ${AgeUser} лет и вы проживаете в городе ${CityUser} по адресу ${AdresUser}. Если все данные верны подвердите отправку формы и за вами выедет машина военкомата.`;
+alert(present);
+
+console.log(present);
+
